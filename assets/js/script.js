607d92292a7bec458dbb11702e22486435b7aacb
chloejones9464 <jones.chloe@live.co.uk>
2025-03-25 16:52:08 +0000

    Grab elements by Id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-document.addEventListener('DOMContentLoaded', setup);
-
+window.addEventListener('DOMContentLoaded', setup);
 
 function setup() {
     document.getElementById("playGame").addEventListener("click", playGame);
-    document.getElementById("numberOfGames").removeEventListener("change", updateNumberOfGames);    
-    document.getElementById("closeGame").addEventListener("click", closeGame); 
+    document.getElementById("numberOfGames").removeEventListener("change", updateNumberOfGames);
 }
 
+// Changes divs to display the game content
 function playGame() {
-   document.getElementById("playGame").removeEventListener("click", playGame);    
-   document.getElementById("closeGame").removeEventListener("click", closeGame); 
+    document.getElementById("playGame").removeEventListener("click", playGame);
+    document.getElementById("closeGame").removeEventListener("click", closeGame);
 
     document.getElementById("firstDiv").innerHTML = /*html*/ `
     <div id="navbar">        
@@ -18,11 +17,11 @@ function playGame() {
         class="box">Rules</button></span>
     </div>
     <div id="gamePageHeading"><h3 class="display-4">Who's getting the next round?</h3></div>
-    <div class="box" id="scores">
-        <span>Your Wins: <span id="playerScore">0</span></span> <br>
-        <span>Your mate's wins: <span id="computerScore">0</span></span> <br>
-        <span>Draws: <span id="drawScore">0</span></span>
-    </div>`;
+            <div class="box"><span>Number of Games: <span id="numOfGames">0</span></span> <br>
+                <span>Your Wins: <span id="playerScore">0</span></span> <br>
+                <span>Your mate's wins: <span id="computerScore">0</span></span> <br>
+                <span>Draws: <span id="drawScore">0</span></span>
+            </div>`;
 
     document.getElementById("secondDiv").innerHTML = /*html*/ `
     <div id="computerSection">
@@ -36,12 +35,29 @@ function playGame() {
         <div id="rock"><img src="assets/images/rock-hand-2.webp" /></div>
         <div id="paper"><img src="assets/images/paper-hand-2.webp"></div>
         <div id="scissors"><img src="assets/images/scissors-hand-2.webp"></div>
-    </div>`;    
+    </div>`;
 };
 
+//Game logic
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let drawScore = 0;
+    let moves = 0;
 
 
+    //game function
+    function gamePlay() {
+        //grabbing the elements
+        const rock = document.getElementById("rock");
+        const paper = document.getElementById("paper");
+        const scissors = document.getElementById("scissors");
+        const playerOptions = [rock, paper, scissors];
+        const computerOptions = ["rock", "paper", "scissors"];
+        const playerScore = document.getElementById("playerScore");
+        const computerScore = document.getElementById("computerScore");
+        const drawScore = document.getElementById("drawScore");
 
-
-
-
+    }
+}
+            
