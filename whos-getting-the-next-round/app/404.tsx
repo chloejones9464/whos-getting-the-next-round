566a091f386478d4751fcd60d50ade27f45f8ed8
chloejones9464 <jones.chloe@live.co.uk>
2025-04-12 11:33:19 +0100

    Add style to the error 404 page

---
+++ whos-getting-the-next-round/app/404.tsx
@@ -0,0 +1,20 @@
+// This is a 404 page that will be displayed when the user tries to access a page that does not exist
+import React from 'react';
+import '.custom404.css';
+import { useRouter } from 'next/router';
+
+export default function custom404() {
+    const router = useRouter();
+
+    const mainMenu = () => {
+        router.push('/'); // Redirect to the main menu page
+    };
+
+    return <div className='styles.errorContainer'>
+    <h1>404</h1>
+    <div>Ops! looks like you've gone to the wrong pub!</div>
+    <div>Don't worry, I'll give you a lift</div>
+    <button type="button" onClick={mainMenu} className='styles.errorMainMenuButton' >Jump in!!
+        (Main Menu)</button>
+    </div>
+  }
